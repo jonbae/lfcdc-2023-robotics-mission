2d62bf2431155edb927d01089a5c49f8a966a851
Jonathan Bae <jonbae.mingun@gmail.com>
2023-08-24 22:43:09 -0400

    counter and maxpoints

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,82 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-  const [totalPoints, setTotalPoints]  = useState(0); 
-  const missionStates = [
-    {title: "mission 1", description: "this is the first mission", counter: 0}, 
-    {title: "mission 2", description: "this is the first mission", counter: 0}, 
-    {title: "mission 3", description: "this is the first mission", counter: 0}, 
-    {title: "mission 4", description: "this is the first mission", counter: 0}, 
-    {title: "mission 5", description: "this is the first mission", counter: 0}, 
-    {title: "FINAL MISSION", description: "this is the first mission", counter: 0},  
-  ]
+	const [totalPoints, setTotalPoints] = useState(0);
+	const [missionStates, setMissionStates] = useState([
+		{
+			title: "mission 1",
+			description: "this is the 1st mission.",
+			counter: 0,
+			pointLimit: 6,
+		},
+		{
+			title: "mission 2",
+			description: "this is the 2nd mission.",
+			counter: 0,
+			pointLimit: 8,
+		},
+		{
+			title: "mission 3",
+			description: "this is the 3rd mission",
+			counter: 0,
+			pointLimit: 6,
+		},
+		{
+			title: "mission 4",
+			description: "this is the 4th mission",
+			counter: 0,
+			pointLimit: 6,
+		},
+		{
+			title: "FINAL MISSION",
+			description: "this is the 5th and FINAL mission",
+			counter: 0,
+			pointLimit: 80,
+		},
+	]);
 
-  const renderMissions = () => {
-    return missionStates.map( ( missionState, index) => <div className='font-bold underline' key={index}>
-      <p>{missionState.title}</p>
-      <p>{missionState.description}</p>
-      <p>{missionState.counter}</p>
-        {/* max point limit  */}
-    </div>)
-  }
-  
-  return (
-    <main>
-      <div>
-        {totalPoints}
+	const handleCounterChange = (i, symbol) => {
+		let temp = 0;
+		const newMissionStates = missionStates.map((missionState, index) => {
+			if (i === index) {
+				if (symbol === "+" && missionState.counter < missionState.pointLimit)
+					missionState.counter++;
+				if (symbol === "-" && missionState.counter > 0) missionState.counter--;
+			}
+			temp += missionState.counter;
+			return missionState;
+		});
+		setMissionStates(newMissionStates);
+		setTotalPoints(temp);
+	};
 
+	const renderMissions = () => {
+		return missionStates.map((missionState, index) => (
+			<div className="border-2 border-red-400 border-solid m-8" key={index}>
+				<p>{missionState.title}</p>
+				<p>
+					{missionState.description} The max number of points is{" "}
+					{missionState.pointLimit}
+				</p>
+				<div className="flex justify-around items-center">
+					<button onClick={() => handleCounterChange(index, "-")}>-</button>
+					<p>{missionState.counter}</p>
+					<button onClick={() => handleCounterChange(index, "+")}>+</button>
+				</div>
+			</div>
+		));
+	};
 
-        {renderMissions()}
-      </div>
-    </main>
-  )
+	return (
+		<main>
+			<div>
+				Score: {totalPoints}
+				{renderMissions()}
+			</div>
+		</main>
+	);
 }
 
-export default App
+export default App;
